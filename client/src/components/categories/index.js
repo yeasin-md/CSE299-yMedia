ed78ab6add397547416437a4f9e5bc5b2287752f
Md. Yeasin Hossain <80535880+yeasin-md@users.noreply.github.com>
2025-12-08 02:44:19 +0600

    Delete client/src directory

--- client/src/components/categories/index.js
+++
@@ -1,4 +0,0 @@
-export { default as CategoryPage } from "./CategoryPage";
-export { default as CategoryBanner } from "./CategoryBanner";
-export { default as CategoryContent } from "./CategoryContent";
-
--- client/src/container/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Footer.scss";
-const Footer = () => {
-  return <div className="footer">©2025 yMedia. All Rights Reserved.</div>;
-};
-
-export default Footer;
